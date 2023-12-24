b7c5be260e4787f9e118338d724a4c1403118f07
vipex <101529155+vipexv@users.noreply.github.com>
2023-12-24 04:01:53 +0100

    refactor: cleaning up code

--- shared/utils.lua
+++ shared/utils.lua
@@ -7,32 +7,6 @@ function UIMessage(action, data)
   })
 end
 
----@class ZoneInfo
----@field blip table
----@field cancelDistance number
----@field cooldown integer
----@field drawText table
----@field hacking boolean
----@field interactButton integer
----@field interactDistance number
----@field label string
----@field marker table
----@field name string
----@field requireGun boolean
----@field requiredPolice integer
----@field rewardAmount table
----@field timeToRob integer
----@field vec3 vector3
----@field viewDistance number
-
--- function ShowHelpText(coords, text)
---   AddTextEntry('HelpText', text)
---   SetFloatingHelpTextWorldPosition(1, coords)
---   SetFloatingHelpTextStyle(1, 1, 2, -1, 3, 0)
---   BeginTextCommandDisplayHelp('HelpText')
---   EndTextCommandDisplayHelp(2, false, false, -1)
--- end
-
 function ShowFloatingText(coords, msg)
   AddTextEntry('floatingTextNotification', msg)
   ---@diagnostic disable-next-line: missing-parameter
@@ -41,8 +15,6 @@ function ShowFloatingText(coords, msg)
   BeginTextCommandDisplayHelp('floatingTextNotification')
   EndTextCommandDisplayHelp(2, false, false, -1)
 end
-
-local currentResourceName = GetCurrentResourceName()
 
 function AddBlip(coords, text, sprite, colour, scale, removeBlip)
   local blip = AddBlipForCoord(coords.x, coords.y, coords.z)
@@ -64,6 +36,8 @@ function AddBlip(coords, text, sprite, colour, scale, removeBlip)
   end
   return blip
 end
+
+local currentResourceName = GetCurrentResourceName()
 
 function Debug(...)
   if not Config.Debug then return end
