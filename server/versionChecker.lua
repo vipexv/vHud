2c7ce7a85e0414d675fb81be433a11823340e55f
vipex <101529155+vipexv@users.noreply.github.com>
2023-12-30 19:43:36 +0100

    refactor: update to newest version

--- server/versionChecker.lua
+++ server/versionChecker.lua
@@ -9,7 +9,7 @@ end
 local versionCheck = function(repository)
     local resource = GetInvokingResource() or GetCurrentResourceName()
 
-    local currentVersion = 'v1.1.4'
+    local currentVersion = 'v1.1.5'
 
     if currentVersion then
         currentVersion = currentVersion:match('%d+%.%d+%.%d+')
