42d3a4f4ef471890b158a68ac380dfa45b0eb972
vipex <101529155+vipexv@users.noreply.github.com>
2023-12-30 20:03:36 +0100

    style: fixed an issue with the seatbelt animation

--- config.lua
+++ config.lua
@@ -2,11 +2,11 @@ Config = {
   ["Debug"] = false,
   ["Server Name"] = "Server Name",       -- The Server name that is going to display at the top right.
   ["Footer"] = "discord.gg/server_link", -- The text that will display below the server name, usually a discord link
-  ["Framework"] = "standalone",          -- standalone, esx, qb, vrp
+  ["Framework"] = "esx",                 -- standalone, esx, qb, vrp
   ["Framework Options"] = {              -- Enable these only if using a framework.
     ["Status"] = false,                  -- Hunger and thirst [esx, qb, vrp]
     ["Multi Character"] = false,         -- Basiclly so the HUD doesn't appear once the player is in the character selector, only appears if they have selected a character/loaded in (QB/ESX)
-    ["Seatbelt"] = false,                -- [ESX, QB]
+    ["Seatbelt"] = true,                 -- [ESX, QB]
   },
   ["Player Slots"] = 200,                -- The player slots for your server (Convar: sv_maxclients)
   ["Default Settings"] = {               -- The default hud settings for the player once they load in for the first time.
--- web/src/components/CarHud.tsx
+++ web/src/components/CarHud.tsx
@@ -6,7 +6,7 @@ import "../App.css";
 import { Cog, Fuel, Gauge } from "lucide-react";
 import { animateNumber } from "../utils/animateNumber";
 import { ConfigInterface } from "@/App";
-import { Transition } from "@mantine/core";
+import { Button, Transition } from "@mantine/core";
 import { FaUserAltSlash } from "react-icons/fa";
 import { SettingsInterface } from "@/App";
 
@@ -41,22 +41,33 @@ const CarHud: React.FC<props> = ({ userSettings, scriptConfig }) => {
   });
   return (
     <>
+      {process.env.NODE_ENV === "development" && (
+        <>
+          <div className="flex flex-col justify-center items-start ml-5">
+            <Button
+              className="bg-black rounded p-2 mt-10"
+              onClick={() => {
+                const updatedData: VehData = {
+                  ...vehicleData,
+                  isSeatbeltOn: !vehicleData.isSeatbeltOn,
+                };
+                setVehicleData(updatedData);
+              }}
+            >
+              Toggle seatbelt{" "}
+            </Button>
+          </div>
+        </>
+      )}
       <div
-        className="absolute top-[91vh] left-2/4 -translate-x-2/4 -translate-y-2/4 font-inter text-white"
+        className="absolute top-[90vh]  left-2/4 -translate-x-2/4 -translate-y-2/4 font-inter text-white"
         style={{
           opacity: userSettings.transparency
             ? `${userSettings.transparency}%`
             : "100%",
         }}
       >
-        <div
-          className={`flex flex-col justify-center items-center ${
-            scriptConfig["Framework Options"].Seatbelt &&
-            !vehicleData.isSeatbeltOn
-              ? "mb-8"
-              : ""
-          }`}
-        >
+        <div className="flex justify-center items-center h-[40px]">
           {scriptConfig["Framework Options"].Seatbelt && (
             <>
               <Transition
@@ -76,42 +87,42 @@ const CarHud: React.FC<props> = ({ userSettings, scriptConfig }) => {
               </Transition>
             </>
           )}
-          <div
-            className={`flex flex-row bg-black opacity-80 rounded-[2px] p-2 px-1 font-bold justify-between scale-90 ${
-              userSettings.hudMode.toString() == "2" ? "skew-x-6" : ""
-            }`}
-          >
-            <div className="flex p-1 gap-2 justify-center items-center">
-              <motion.p
-                initial={{
-                  y: 20,
-                }}
-                animate={{
-                  y: 0,
-                }}
-                key={vehicleData.gear}
-              >
-                {vehicleData.gear}
-              </motion.p>
-              <Cog size={16} />
-            </div>
-            <div className="flex justify-center items-center flex-col mr-5 ml-5">
-              <p className="" id="vehSpeed">
-                0
-              </p>
-              <p className="text-xs opacity-50">
-                {userSettings.measurementSystem === "MPH" ? "MP/H" : "KM/H"}
-              </p>
-            </div>
-            <div className="flex p-1 gap-2 justify-center transition-all">
-              <Fuel size={16} strokeWidth={2} className="place-self-center" />
-              <div
-                className={`w-[7px] rounded-[2px] max-h-full bg-blue-600 transition-all place-self-end`}
-                style={{
-                  height: `${vehicleData.fuel}%`,
-                }}
-              ></div>
-            </div>
+        </div>
+        <div
+          className={`flex flex-row bg-black opacity-80 rounded-[2px] p-2 px-1 font-bold justify-between scale-90 ${
+            userSettings.hudMode.toString() == "2" ? "skew-x-6" : ""
+          }`}
+        >
+          <div className="flex p-1 gap-2 justify-center items-center">
+            <motion.p
+              initial={{
+                y: 20,
+              }}
+              animate={{
+                y: 0,
+              }}
+              key={vehicleData.gear}
+            >
+              {vehicleData.gear}
+            </motion.p>
+            <Cog size={16} />
+          </div>
+          <div className="flex justify-center items-center flex-col mr-5 ml-5">
+            <p className="" id="vehSpeed">
+              0
+            </p>
+            <p className="text-xs opacity-50">
+              {userSettings.measurementSystem === "MPH" ? "MP/H" : "KM/H"}
+            </p>
+          </div>
+          <div className="flex p-1 gap-2 justify-center transition-all">
+            <Fuel size={16} strokeWidth={2} className="place-self-center" />
+            <div
+              className={`w-[7px] rounded-[2px] max-h-full bg-blue-600 transition-all place-self-end`}
+              style={{
+                height: `${vehicleData.fuel}%`,
+              }}
+            ></div>
           </div>
         </div>
       </div>
