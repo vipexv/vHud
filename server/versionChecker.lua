3a13e041f776451f8aec6cdc1ef89fd33c1df1cb
vipex <101529155+vipexv@users.noreply.github.com>
2024-01-22 00:25:38 +0100

    revert: remove unused command

--- server/versionChecker.lua
+++ server/versionChecker.lua
@@ -9,7 +9,7 @@ end
 local versionCheck = function(repository)
     local resource = GetInvokingResource() or GetCurrentResourceName()
 
-    local currentVersion = 'v1.1.9'
+    local currentVersion = 'v1.2.0'
 
     if currentVersion then
         currentVersion = currentVersion:match('%d+%.%d+%.%d+')
